37fe47028d6e12d0827b7590927a404af2c76d44
itsSilver <thesilvervevo@gmail.com>
2021-05-04 16:17:04 +0200

    added avatar

--- components/Dashboard/Menu.vue
+++ components/Dashboard/Menu.vue
@@ -25,14 +25,7 @@
           @blur="hideProfileNav"
         >
           <span class="relative flex-shrink-0">
-            <img
-              class="w-7 h-7 rounded-full"
-              src="https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
-              alt="profile"
-            />
-            <span
-              class="absolute right-0 -mb-0.5 bottom-0 w-2 h-2 rounded-full bg-green-500 border border-white dark:border-gray-900"
-            ></span>
+            <img class="w-7 h-7 rounded-full" :src="userImage" alt="profile" />
           </span>
           <span class="ml-2">{{ this.$auth.user.username }}</span>
           <svg
@@ -65,13 +58,14 @@
               aria-orientation="vertical"
               aria-labelledby="user-menu"
             >
-              <a
-                href="#"
+              <nuxt-link
                 class="flex px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 uppercase"
-                role="menuitem"
+                v-for="(menu, index) in userMenu"
+                :key="index"
+                :disabled="menu.disabled"
+                :to="localePath(`${menu.url}`)"
+                >{{ menu.name }}</nuxt-link
               >
-                Profili
-              </a>
             </div>
           </div>
         </transition>
@@ -89,6 +83,23 @@ export default {
   data() {
     return {
       openProfile: false,
+      userImage:
+        `http://localhost:1337${this.$auth.user.avatar.formats.thumbnail.url}` ||
+        '../../avatar.png',
+      userMenu: [
+        {
+          name: `${this.$t('dashboard.user-menu.profile')}`,
+          url: '/profile',
+        },
+        {
+          name: `${this.$t('dashboard.user-menu.payments')}`,
+          url: '/payments',
+        },
+        {
+          name: `${this.$t('dashboard.user-menu.logout')}`,
+          url: '/logout',
+        },
+      ],
     }
   },
   methods: {
